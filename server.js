c12ee3cc448261fed02d2cb38bc53a3f22604810
Brianna Gaines <briannagaines31@gmail.com>
2022-04-07 18:01:32 -0600

    adds database of drinks

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const drinks = require("./models/drinks.js")
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req,res)=>{
     res.send('welcome to the Gitpub app')
 })
 
-
+app.get("/drinks/", (req,res)=>{
+    res.send(drinks)
+})
 
 
 
